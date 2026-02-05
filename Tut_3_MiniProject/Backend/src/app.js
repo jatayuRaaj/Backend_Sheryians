3513fc03171567b445873716a16af54dba98cac2
Gulshan kumar <3435tonighttillthelights@gmail.com>
2026-02-06 01:01:03 +0530

    commiting miniProject frontend before moving from jatayu to gulshan branch

--- Tut_3_MiniProject/Backend/src/app.js
+++ Tut_3_MiniProject/Backend/src/app.js
@@ -2,12 +2,13 @@ const express = require('express');
 const multer = require('multer');
 const uploadFile = require('../services/storage.service');
 const postModel = require('../models/post.model');
+const cors = require('cors');
 
 const app = express();
 const upload = multer({ storage: multer.memoryStorage() })
 
 app.use(express.json());
-
+app.use(cors());
 app.get("/home", (req, res) => {
     res.send('yo yo');
 })
@@ -17,7 +18,7 @@ app.post("/create-post", upload.single('image'), async (req, res) => {
     const result = await uploadFile(req.file.buffer);
 
     const post = await postModel.create({
-        image_link : result.url,
+        image_link: result.url,
         caption: req.body.caption
     })
 
@@ -33,19 +34,19 @@ app.get("/posts", async (req, res) => {
     res.status(200).json({
         message: "posts fetched successfully",
         posts: posts,
-        link : posts.image
+        link: posts.image
     })
- 
+
 })
 
-app.delete("/delete/:id", async (req, res)=>{
+app.delete("/delete/:id", async (req, res) => {
     const id = req.params.id;
     await postModel.findOneAndDelete({
-        _id : id 
+        _id: id
     })
     res.status(200).json({
-        message : "post deleted successfully.",
-        deleted_id  : id
+        message: "post deleted successfully.",
+        deleted_id: id
     })
 })
 module.exports = app
