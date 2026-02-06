af415871c25f4ac25d88f4dfd5db437bf55334cb
Gulshan kumar <3435tonighttillthelights@gmail.com>
2026-02-07 01:22:40 +0530

    authentication completed

--- Tut_4_Authentication/routes/auth.routes.js
+++ Tut_4_Authentication/routes/auth.routes.js
@@ -1,11 +1,17 @@
 const express = require('express');
-const registerUser = require('../controllers/auth.controller')
-
+// const registerUser = require('../controllers/auth.controller')
+const authController = require('../controllers/auth.controller');
+const userModel = require('../models/user.model');
 
 const router = express.Router();
 
-router.post("/register", (req, res) => {
-
+router.post("/register", authController.registerUser );
+router.get("/test", (req, res)=>{
+    console.log("cookie", req.cookies);
+    res.json({
+        message : "cookies fetched successfully",
+        cookies : req.cookies
+    })
 })
 
 module.exports = router;
--- Tut_4_Authentication/src/app.js
+++ Tut_4_Authentication/src/app.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const authRoutes = require('../routes/auth.routes');
+const cookieParser = require('cookie-parser');
+const postRoutes = require('../routes/post.routes');
 
 
 const app = express();
-app.use(express.json());
 
-// app.get('/home', (req, res) => {
-//     res.send('<h1>This is Home Page</h1>')
-// })
+app.use(express.json());
+app.use(cookieParser());
+
+
+app.use("/api/auth", authRoutes);    // when we need to access the api we will write api/auth/controllersName
+app.use("/api/auth", postRoutes);
 
 
 module.exports = app;
