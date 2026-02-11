8c59d07dd0dc272dfd3d4ddc4f4bc479de5a0d79
jatayuRaaj <3435tonighttillthelights@gmail.com>
2026-02-11 12:00:41 +0530

    Update auth.routes.js

--- Tut_5_Role_base_Auth/Backend/routes/auth.routes.js
+++ Tut_5_Role_base_Auth/Backend/routes/auth.routes.js
@@ -3,7 +3,11 @@ const { registerUser, loginUser, logoutUser } = require('../controllers/auth.con
 
 const router = express.Router();
 
+// here we just use router acquired from express to post or get and tell the where to get from or post to like "/home" or "'about" and then tell it which contorller
+// to call at this route
+
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.post('/logout', logoutUser);
+
 module.exports = router;
