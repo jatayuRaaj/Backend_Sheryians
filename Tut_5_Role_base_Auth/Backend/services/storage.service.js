6e9761951cd85a7f2d05d6c0e75e7528dd047ff7
jatayuRaaj <3435tonighttillthelights@gmail.com>
2026-02-11 11:48:10 +0530

    Update storage.service.js

--- Tut_5_Role_base_Auth/Backend/services/storage.service.js
+++ Tut_5_Role_base_Auth/Backend/services/storage.service.js
@@ -4,6 +4,8 @@ require('dotenv').config();
 const ImageKitClient = new ImageKit({
     privateKey: process.env.IMAGEKTI_PRIVATE_KEY
 })
+// we have set up the image kit above with private_key and now we will make a function that uplaods file and takes three things and upadates in the imageKit
+// that is file itself than its name and folder in which it will be set.
 
 async function uploadFile(file) {
     const result = ImageKitClient.files.upload({
